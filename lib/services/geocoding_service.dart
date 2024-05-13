675d73d685f6f72886f44ee4380f64a7c4262282
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2024-05-12 23:49:26 -0400

    Update geocoding_service.dart

--- lib/services/geocoding_service.dart
+++ lib/services/geocoding_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GeocodingService {
-  final String apiKey = '2992c0652da8302548c78a7ada23316b';
+  final String apiKey = 'YOUR_API_KEY';
   final String baseUrl = 'http://api.openweathermap.org/geo/1.0/direct';
 
   Future<List<String>> fetchCitySuggestions(String query) async {
